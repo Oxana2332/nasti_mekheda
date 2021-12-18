63464fe7af17301f6b805fa47bf5fe30e1784e59
Oxana2332 <far_north@ro.ru>
2021-12-18 17:02:28 +0300

    правильная ссылка на jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,9 @@ $(document).ready(function() {
 $('.header__burger').click(function(event) {
 $('.header__burger,.header__menu').toggleClass('active');
 $('body').toggleClass('lock')
-
 })
-
 });
+
 $(document).ready(function() {
     $('.beautyslider').slick({
  slidesToShow:3,
