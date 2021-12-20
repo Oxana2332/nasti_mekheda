5aa5518c59ea691539df8aaf7dfef831ab9bdc90
Oxana2332 <far_north@ro.ru>
2021-12-20 12:59:50 +0300

    закрывающийся header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
-$('.header__burger').click(function(event) {
-$('.header__burger,.header__menu').toggleClass('active');
-$('body').toggleClass('lock')
-})
+  let isOpen = false;
+  function checkIsOpen(newIsOpen){
+    if (newIsOpen){
+      $('.header__burger,.header__menu').addClass('active');
+      $('body').addClass('lock');
+    } else {
+      $('.header__burger,.header__menu').removeClass('active');
+      $('body').removeClass('lock');
+    }
+  }
+
+  $(".header__link").click(
+    function(event){
+      event.preventDefault();
+      const targetPage = event.target.href
+        .split('/')
+        .at(-1)
+        .split('#')[0];
+      const currentPage = window.location.pathname.split('/').at(-1).split('#')[0];
+      if (targetPage === currentPage){
+        isOpen = false;
+        checkIsOpen(isOpen);
+      }
+      window.location.replace(event.target.href);
+    }
+  )
+  $('.header__burger').click(function(event) {
+    isOpen = !isOpen 
+    checkIsOpen(isOpen);
+  })
 });
 
 $(document).ready(function() {
