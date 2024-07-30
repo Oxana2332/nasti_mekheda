2c26ad8d8f03ba9b383e981b451a62e0c2e5237b
Oxana Rozhkova <far_north@ro.ru>
2024-07-30 22:15:02 +0300

    слайдеры

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,68 @@
-$(document).ready(function() {
-  let isOpen = false;
-  function checkIsOpen(newIsOpen){
-    if (newIsOpen){
-      $('.header__burger,.header__menu').addClass('active');
-      $('body').addClass('lock');
-    } else {
-      $('.header__burger,.header__menu').removeClass('active');
-      $('body').removeClass('lock');
-    }
-  }
+$(document).ready(function () {
+	let isOpen = false;
+	function checkIsOpen(newIsOpen) {
+		if (newIsOpen) {
+			$('.header__burger,.header__menu').addClass('active');
+			$('body').addClass('lock');
+		} else {
+			$('.header__burger,.header__menu').removeClass('active');
+			$('body').removeClass('lock');
+		}
+	}
 
-  $(".header__link").click(
-    function(event){
-      event.preventDefault();
-      const targetPage = event.target.href
-        .split('/')
-        .at(-1)
-        .split('#')[0];
-      const currentPage = window.location.pathname.split('/').at(-1).split('#')[0];
-      if (targetPage === currentPage){
-        isOpen = false;
-        checkIsOpen(isOpen);
-      }
-      window.location.replace(event.target.href);
-    }
-  )
-  $('.header__burger').click(function(event) {
-    isOpen = !isOpen 
-    checkIsOpen(isOpen);
-  })
+	$('.header__link').click(function (event) {
+		event.preventDefault();
+		const targetPage = event.target.href.split('/').at(-1).split('#')[0];
+		const currentPage = window.location.pathname
+			.split('/')
+			.at(-1)
+			.split('#')[0];
+		if (targetPage === currentPage) {
+			isOpen = false;
+			checkIsOpen(isOpen);
+		}
+		window.location.replace(event.target.href);
+	});
+	$('.header__burger').click(function (event) {
+		isOpen = !isOpen;
+		checkIsOpen(isOpen);
+	});
 });
 
-$(document).ready(function() {
-    $('.beautyslider').slick({
- slidesToShow:3,
- speed:100,
- easing:'ease',
- draggable:false,
- swipe:true,
- touchTheshold:10,
- adaptiveHeight:true,
-centerMode:true,
-responsive: [
-    {
-      breakpoint: 468,
-      settings: {
-        slidesToShow: 1,
-      }
-    }]
-    })
-})
+$(document).ready(function () {
+	$('.beautyslider').slick({
+		slidesToShow: 3,
+		speed: 100,
+		easing: 'ease',
+		draggable: false,
+		swipe: true,
+		touchTheshold: 10,
+		adaptiveHeight: true,
+		centerMode: true,
+		responsive: [
+			{
+				breakpoint: 468,
+				settings: {
+					slidesToShow: 1,
+				},
+			},
+		],
+	});
+});
 
-$(document).ready(function() {
-    $('.brideslider').slick({
- slidesToShow:1,
- speed:100,
- easing:'ease',
- draggable:false,
- swipe:true,
- touchTheshold:10,
- adaptiveHeight:true,
-    })
-})
+$(document).ready(function () {
+	$('.brideslider').slick({
+		slidesToShow: 1,
+		speed: 100,
+		easing: 'ease',
+		draggable: false,
+		swipe: true,
+		touchTheshold: 10,
+		adaptiveHeight: true,
+	});
+});
 
-$(document).ready(function() {
-    $('.backstageslider').slick({
- slidesToShow:1,
- speed:100,
- easing:'ease',
- draggable:false,
- swipe:true,
- touchTheshold:10,
- adaptiveHeight:true,
-centerMode:true
-    })
-})
+//backstageslider
+function onSelectImage(url) {
+	document.getElementById('mainImage').src = url;
+}
